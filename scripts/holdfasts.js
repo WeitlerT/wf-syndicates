8f4341002f30a9b9e2bf291b9310d0f8cf4e0113
Tyler <tweitler@gmail.com>
2024-03-11 19:30:55 -0400

    Added holdfasts page/items

---
+++ scripts/holdfasts.js
@@ -0,0 +1,51 @@
+// IMPORTS
+import { getAvgPlat } from "./main.js";
+import { fetchDataAndUpdatePlat } from "./main.js";
+import { populatePage } from "./main.js";
+import { convertToSnakeCase } from "./main.js";
+import { showLoadingIndicator } from "./main.js";
+import { hideLoadingIndicator } from "./main.js";
+import { sortByRatioDescending } from "./main.js";
+
+// Fetch button, every page will have one of these
+const fetchBtn = document.querySelector('.fetchDataBtn')
+fetchBtn.addEventListener('click', function() {
+    // Replace list with whatever list you have for page
+    const holdfastsPromises = holdfastsList.map(item => fetchDataAndUpdatePlat(item));
+
+    // Promise for our fetch function in main.js
+    Promise.all(holdfastsPromises)
+        .then(() => {
+            // All fetch requests completed, and plat fields are updated
+            console.log(holdfastsList);
+            let sortedList = sortByRatioDescending(holdfastsList)
+            populatePage(sortedList, 'cardContainer')
+        })
+        .then (() =>{
+            hideLoadingIndicator()
+        })
+        .catch(error => {
+            // Handle errors if any of the fetch requests fail
+            console.error('Error in fetching data:', error);
+            hideLoadingIndicator()
+        });
+})
+
+// Big pretty list goes here
+let holdfastsList = [
+    { name: 'Eternal Eradicate', plat: 0, standing: 5000, url: convertToSnakeCase('Eternal Eradicate'), rank: 5 },
+    { name: 'Cascadia Accuracy', plat: 0, standing: 5000, url: convertToSnakeCase('Cascadia Accuracy'), rank: 5 },
+    { name: 'Fractalized Reset', plat: 0, standing: 5000, url: convertToSnakeCase('Fractalized Reset'), rank: 5 },
+    { name: 'Molt Vigor', plat: 0, standing: 5000, url: convertToSnakeCase('Molt Vigor'), rank: 5 },
+    { name: 'Emergence Savior', plat: 0, standing: 5000, url: convertToSnakeCase('Emergence Savior'), rank: 5 },
+    { name: 'Eternal Onslaught', plat: 0, standing: 5500, url: convertToSnakeCase('Eternal Onslaught'), rank: 5 },
+    { name: 'Cascadia Flare', plat: 0, standing: 5500, url: convertToSnakeCase('Cascadia Flare'), rank: 5 },
+    { name: 'Cascadia Empowered', plat: 0, standing: 7500, url: convertToSnakeCase('Cascadia Empowered'), rank: 5 },
+    { name: 'Molt Efficiency', plat: 0, standing: 7500, url: convertToSnakeCase('Molt Efficiency'), rank: 5 },
+    { name: 'Emergence Renewed', plat: 0, standing: 7500, url: convertToSnakeCase('Emergence Renewed'), rank: 5 },
+    { name: 'Molt Reconstruct', plat: 0, standing: 8500, url: convertToSnakeCase('Molt Reconstruct'), rank: 5 },
+    { name: 'Eternal Logistics', plat: 0, standing: 8500, url: convertToSnakeCase('Eternal Logistics'), rank: 5 },
+    { name: 'Cascadia Overcharge', plat: 0, standing: 10000, url: convertToSnakeCase('Cascadia Overcharge'), rank: 5 },
+    { name: 'Emergence Dissipate', plat: 0, standing: 10000, url: convertToSnakeCase('Emergence Dissipate'), rank: 5 },
+    { name: 'Molt Augmented', plat: 0, standing: 10000, url: convertToSnakeCase('Molt Augmented'), rank: 5 },
+];
--- scripts/home.js
+++ scripts/home.js
@@ -2,7 +2,12 @@ const steelBtn = document.getElementById('steelMeridian')
 steelBtn.addEventListener('click', function() {
       window.location.href = 'pages/steel.html';
 })
-    
+
+const holdfastsBtn = document.getElementById('holdfasts')
+holdfastsBtn.addEventListener('click', function() {
+    window.location.href = 'pages/holdfasts.html';
+})
+
 const caviaBtn = document.getElementById('cavia')
 caviaBtn.addEventListener('click', function() {
     window.location.href = 'pages/cavia.html';
